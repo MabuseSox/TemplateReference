cde452dad5d8cb8a35406fab8521397183442387
MabuseSerame <seramemabuse1@gmail.com>
2022-06-09 13:42:44 +0200

    customerList added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DemoComponent } from './demo/demo.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DemoComponent
+    DemoComponent,
+    CustomerListComponent
   ],
   imports: [
     BrowserModule,
